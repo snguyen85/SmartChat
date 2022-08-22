83ac9875663d492ca1fecf5cd8cde45a428c27d5
snguyen85 <steve.nguyen404@gmail.com>
2022-08-22 12:46:25 -0600

    Fix for Unexpected Case Serialzation
    - Add json property name to force a serialization convention
    - Prevent the errors list from being null

--- Shared/Models/LoginResult.cs
+++ Shared/Models/LoginResult.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SmartChat.Shared.Models
 {
     public class LoginResult
     {
+        [JsonPropertyName("Successful")]
         public bool Successful { get; set; }
+        [JsonPropertyName("Error")]
         public string Error { get; set; }
+        [JsonPropertyName("Token")]
         public string Token { get; set; }
     }
 }
--- Shared/Models/RegisterResult.cs
+++ Shared/Models/RegisterResult.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SmartChat.Shared.Models
 {
     public class RegisterResult
     {
+        [JsonPropertyName("Successful")]
         public bool Successful { get; set; }
+        [JsonPropertyName("Errors")]
         public IEnumerable<string> Errors { get; set; }
     }
 }
